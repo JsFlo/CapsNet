5c0677434aa012061255f7d8dd1910620dfee9b6
Jose Flores <jsflores480@gmail.com>
2017-12-09 18:52:08 -0600

    renames for clarity and pulls out transform method

--- train.py
+++ train.py
@@ -22,39 +22,14 @@ N_ITERATIONS_VALIDATION = 1  # mnist.validation.num_examples // TRAINING_BATCH_S
 BEST_LOSS_VAL = np.infty
 
 
-def train():
-    input_image_batch = tf.placeholder(shape=[None, 28, 28, 1], dtype=tf.float32)
-    batch_size = tf.shape(input_image_batch)[0]
-
-    digitCaps_postRouting = CapsNetModel.get_model_output(input_image_batch, batch_size)
-    # (?, 1, 10, 16, 1)
-
-    # what we have: 10 16-dimensional vectors
-    # what we want: which digit are you predicting ?
-
-    # normalize to to get 10 scalars (length of the vectors)
-    y_prob = safe_norm(digitCaps_postRouting, axis=-2)
-    # (", 1, 10, 1)
-
-    # get index of longest output vector
-    y_prob_argmax = tf.argmax(y_prob, axis=2)
-    # (?, 1, 1)
-
-    # we have a 1 x 1 matrix , lets just say 1
-    y_pred = tf.squeeze(y_prob_argmax, axis=[1, 2])
-    # (?, )
-
-    # labels
-    y = tf.placeholder(shape=[None], dtype=tf.int64, name="y")
-
-    # paper used special margin loss to detect more than 1 digit in an image (overachievers)
+# paper used special margin loss to detect more than 1 digit in an image (overachievers)
+def get_margin_loss(predicted_digit, digitCaps_postRouting):
     m_plus = 0.9
     m_minus = 0.1
     lambda_ = 0.5
 
-    T = tf.one_hot(y, depth=10, name="T")
+    predicted_one_hot_digit = tf.one_hot(predicted_digit, depth=10)
 
-    # again
     digitCaps_postRouting_safeNorm = safe_norm(digitCaps_postRouting, axis=-2, keep_dims=True)
 
     present_error_raw = tf.square(tf.maximum(0., m_plus - digitCaps_postRouting_safeNorm))
@@ -62,20 +37,18 @@ def train():
     absent_error_raw = tf.square(tf.maximum(0., digitCaps_postRouting_safeNorm - m_minus))
     absent_error = tf.reshape(absent_error_raw, shape=(-1, 10))
 
-    loss = tf.add(T * present_error, lambda_ * (1.0 - T) * absent_error)
+    loss = tf.add(predicted_one_hot_digit * present_error, lambda_ * (1.0 - predicted_one_hot_digit) * absent_error)
     margin_loss = tf.reduce_mean(tf.reduce_sum(loss, axis=1))
+    return margin_loss
 
-    # reconstruction - there will be a loss here too used to compute the final loss
-    # the final loss takes into account the margin loss and the reconstruction loss
 
+def get_reconstruction_loss(mask_with_labels, y, y_pred, digitCaps_postRouting, input_image_batch):
     # first take the 10 16-dimension vectors output and pulls out the [predicted digit vector|correct_label digit vector)
     # (ex. prediction: digit 3 so take the 16-dimension vector and pass it to the decoder)
 
     # make a tensorflow placeholder for choosing based on label or prediction
     # during training pass the correct label digit
     # during inference pass what the model guessed
-    mask_with_labels = tf.placeholder_with_default(False, shape=())
-
     reconstruction_targets = tf.cond(mask_with_labels,  # condition
                                      lambda: y,  # if True
                                      lambda: y_pred)  # if False)
@@ -92,15 +65,52 @@ def train():
 
 
     # Decoder will use the 16 dimension vector to reconstruct the image (28 x 28)
-    n_output = 28 * 28
     reconstruction_loss = Decoder.get_reconstruction_loss(masked_out, input_image_batch)
+    return reconstruction_loss
+
+
+def transform_model_output_to_a_single_digit(digitCaps_postRouting):
+    # what we have: 10 16-dimensional vectors
+    # what we want: which digit are you predicting ?
+
+    # normalize to to get 10 scalars (length of the vectors)
+    y_prob = safe_norm(digitCaps_postRouting, axis=-2)
+    # (", 1, 10, 1)
+
+    # get index of longest output vector
+    y_prob_argmax = tf.argmax(y_prob, axis=2)
+    # (?, 1, 1)
+
+    # we have a 1 x 1 matrix , lets just say 1
+    y_pred = tf.squeeze(y_prob_argmax, axis=[1, 2])
+    return y_pred
+
+
+def train():
+    input_image_batch = tf.placeholder(shape=[None, 28, 28, 1], dtype=tf.float32)
+    batch_size = tf.shape(input_image_batch)[0]
+
+    digitCaps_postRouting = CapsNetModel.get_model_output(input_image_batch, batch_size)
+    # (?, 1, 10, 16, 1)
+
+    single_digit_prediction = transform_model_output_to_a_single_digit(digitCaps_postRouting)
+    # (?, )
+
+    # labels
+    correct_labels_placeholder = tf.placeholder(shape=[None], dtype=tf.int64)
+
+    margin_loss = get_margin_loss(correct_labels_placeholder, digitCaps_postRouting)
+    mask_with_labels = tf.placeholder_with_default(False, shape=())
+
+    reconstruction_loss = get_reconstruction_loss(mask_with_labels, correct_labels_placeholder, single_digit_prediction,
+                                                  digitCaps_postRouting, input_image_batch)
 
     # keep it small
     reconstruction_alpha = 0.0005
     # favor the margin loss with a small weight for reconstruction loss
     final_loss = tf.add(margin_loss, reconstruction_alpha * reconstruction_loss)
 
-    correct = tf.equal(y, y_pred)
+    correct = tf.equal(correct_labels_placeholder, single_digit_prediction)
     accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
     optimizer = tf.train.AdamOptimizer()
     training_op = optimizer.minimize(final_loss)
@@ -117,10 +127,10 @@ def train():
         for epoch in range(N_EPOCHS):
             for iteration in range(1, N_ITERATIONS_PER_EPOCH + 1):
                 X_batch, y_batch = MNIST.train.next_batch(TRAINING_BATCH_SIZE)
-                # Run the training operation and measure the loss:
+                # train and get loss to log
                 _, loss_train = sess.run([training_op, final_loss],
                                          feed_dict={input_image_batch: X_batch.reshape([-1, 28, 28, 1]),
-                                                    y: y_batch,
+                                                    correct_labels_placeholder: y_batch,
                                                     mask_with_labels: True})  # use labels during training for the decoder
 
                 print("\rIteration: {}/{} ({:.1f}%)  Loss: {:.5f}".format(
@@ -129,8 +139,7 @@ def train():
                     loss_train),
                     end="")
 
-            # At the end of each epoch,
-            # measure the validation loss and accuracy:
+            # check against validation set and log it
             loss_vals = []
             acc_vals = []
             for iteration in range(1, N_ITERATIONS_VALIDATION + 1):
@@ -138,7 +147,7 @@ def train():
                 loss_val, acc_val = sess.run(
                     [final_loss, accuracy],
                     feed_dict={input_image_batch: X_batch.reshape([-1, 28, 28, 1]),
-                               y: y_batch})
+                               correct_labels_placeholder: y_batch})
                 loss_vals.append(loss_val)
                 acc_vals.append(acc_val)
                 print("\rEvaluating the model: {}/{} ({:.1f}%)".format(
