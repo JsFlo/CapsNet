be8c1bb89bcc6d114cb2797333dd48914cf97c9d
Jose Flores <jsflores480@gmail.com>
2017-12-10 14:59:09 -0600

    export min decoder will output the masked capsules for the model, no need for two scripts now

--- minimal_decoder/export_minimal_decoder.py
+++ minimal_decoder/export_minimal_decoder.py
@@ -1,7 +1,7 @@
 from __future__ import division, print_function, unicode_literals
 import os
 import tensorflow as tf
-
+import numpy as np
 # puts a bad dependency - can only be run from this directory
 import sys
 
@@ -10,13 +10,24 @@ sys.path.append('../')
 # Makes them look like static method calls (not python style but helps me :)
 import caps_net_model.model as CapsNetModel
 import argparse
+from tensorflow.examples.tutorials.mnist import input_data
+
+MNIST = input_data.read_data_sets("/tmp/data/")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--checkpoint', type=str, required=True,
                     help='Path to CHECKPOINT_NAME file (without ext) ([CHECKPOINT_NAME].meta, [CHECKPOINT_NAME].index, etc).')
 parser.add_argument('--model_output', type=str, required=False, default='./model_output')
-parser.add_argument('--model_name', type=str, required=False, default='model_graph.pb')
+parser.add_argument('--model_name', type=str, required=False, default='model_graph_v2.pb')
+parser.add_argument('--masked_capsules', type=int, required=False, default=30)
 FLAGS = parser.parse_args()
+
+MASKED_OUTPUT_PATH = FLAGS.model_output + "/masked_capsules/"
+MASKED_OUT_NPY_FILENAME = "masked_output.npy"
+
+SOURCE_IMAGES_OUTPUT_PATH = FLAGS.model_output + "/source_images/"
+SOURCE_IMAGES_NPY_FILENAME = "source_images.npy"
+
 
 # CHECKPOINT_PATH = "../checkpoint_with_decoder_tags_67_epoch/my_caps_net"
 # EXPORT_DIR = './model_output'
@@ -25,7 +36,11 @@ def create_dirs_if_not_exists(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
+
 create_dirs_if_not_exists(FLAGS.model_output)
+create_dirs_if_not_exists(MASKED_OUTPUT_PATH)
+create_dirs_if_not_exists(SOURCE_IMAGES_OUTPUT_PATH)
+
 
 def exportGraph(g, W1, B1, W2, B2, W3, B3):
     with g.as_default():
@@ -66,7 +81,7 @@ def restore_export():
     decoder_output, \
     single_digit_prediction, \
     mask_with_labels, \
-    correct_labels_placeholder = CapsNetModel.get_model_output_for_tweak(input_image_batch, batch_size)
+    correct_labels_placeholder, masked_out = CapsNetModel.get_model_output_for_tweak(input_image_batch, batch_size)
 
     all_var = tf.trainable_variables()
     decoder_weights = [var for var in all_var if 'decoder' in var.name]
@@ -75,10 +90,21 @@ def restore_export():
         print("Tensors shapes: {}".format(weight_tensor.shape))
 
     saver = tf.train.Saver()
+    source_images = MNIST.test.images[:FLAGS.masked_capsules].reshape([-1, 28, 28, 1])
 
     with tf.Session() as sess:
         # restore
         saver.restore(sess, FLAGS.checkpoint)
+
+        # save minimal decoder input
+        digitCaps_postRouting_value, decoder_output_value, single_digit_prediction_value, masked_out_value = sess.run(
+            [digitCaps_postRouting, decoder_output, single_digit_prediction, masked_out],
+            feed_dict={input_image_batch: source_images,
+                       correct_labels_placeholder: np.array([], dtype=np.int64)})
+
+        print("Masked capsules shape: {}".format(masked_out_value.shape))
+        np.save(MASKED_OUTPUT_PATH + MASKED_OUT_NPY_FILENAME, masked_out_value)
+        np.save(SOURCE_IMAGES_OUTPUT_PATH + SOURCE_IMAGES_NPY_FILENAME, source_images)
 
         # save out small model
         W1 = decoder_weights[0].eval(sess)
